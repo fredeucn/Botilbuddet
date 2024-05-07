3bce6706aead2ea7033d1de127975e8f96d29fbb
Cowio <10503078@ucn.dk>
2024-05-07 10:37:27 +0200

    Fixed errors
    + added return null for unimplemented methods.
    + added more imports

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import model.Employee;
+import model.EmployeeDB;
+
 public class EmployeeController {
 	private EmployeeDB employeeDB;
 	
 	public Employee findEmployeeById(int id) {
-		
+		return null;
 	}
 }
--- src/controller/PatientController.java
+++ src/controller/PatientController.java
@@ -1,9 +1,13 @@
 package controller;
 
+import model.Patient;
+import model.PatientDB;
+
 public class PatientController {
 	private PatientDB patientDB;
 	
 	public Patient findPatientById(int id) {
+		return null;
 		
 	}
 }
